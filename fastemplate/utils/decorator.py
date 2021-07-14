64f3399accc6f3e0f4e63c10aef5ef579a92356c
Henrique Brandão <brandao.t.henrique@gmail.com>
2021-07-14 16:26:41 -0300

    Update decorator.py

--- fastemplate/utils/decorator.py
+++ fastemplate/utils/decorator.py
@@ -1,31 +1,15 @@
 import time
-from functools import wraps
-
-from fastemplate import logger
-from fastemplate.module.cart import _CART
-from fastemplate.exceptions.cart import CartIdNotFoundException
+import functools
 
 
 def timer(func):
-    """
-    Print the runtime of the decorated function.
-
-    :param func func: func to be decorated
-    :return: wrapped function
-    """
-    @wraps(func)
+    """Print the runtime of the decorated function"""
+    @functools.wraps(func)
     def wrapper_timer(*args, **kwargs):
         start_time = time.perf_counter()    # 1
-        value = func(*args, **kwargs)
+        fun = func(*args, **kwargs)
         end_time = time.perf_counter()      # 2
         run_time = end_time - start_time    # 3
-        logger.info(f'Finished {func.__name__!r} in {run_time:.4f} secs')
-        return value
+        print(f"Finished {func.__name__!r} in {run_time:.4f} secs")
+        return fun
     return wrapper_timer
-
-
-def check_id(id: str):
-    if id in _CART:
-        return True
-    else:
-        raise CartIdNotFoundException(status_code=404, message=f'Cart #{id} does not exist.')
