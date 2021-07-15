33827fd55e915e80445736cb427b41973e844f99
Henrique Brandão <brandao.t.henrique@gmail.com>
2021-07-15 15:14:27 -0300

    Update cart.py

--- fastemplate/exceptions/cart.py
+++ fastemplate/exceptions/cart.py
@@ -1,51 +1,41 @@
 from fastemplate.exceptions import FastemplateBaseException
 
-
-class CartIdAlreadyExistsException(Exception):
+class CartIdAlreadyExistsException(FastemplateBaseException):
     """
     Thrown whenever cart id already exists.
     """
-    def __init__(self, status_code: int, message: str):
-        self.status_code = status_code
-        self.message = message
-        self.name = 'CartIdAlreadyExistsException'
+    def __init__(self, message: str):
+        name = 'CartIdAlreadyExistsException'
+        super().__init__(503, message, name)
 
-
-class CartIdNotFoundException(Exception):
+class CartIdNotFoundException(FastemplateBaseException):
     """
     Thrown whenever cart id does not exist.
     """
-    def __init__(self, status_code: int, message: str):
-        self.status_code = status_code
-        self.message = message
-        self.name = 'CartIdNotFoundException'
+    def __init__(self, message: str):
+        name = 'CartIdNotFoundException'
+        super().__init__(404, message, name)
 
-
-class ItemAlreadyAddedException(Exception):
+class ItemAlreadyAddedException(FastemplateBaseException):
     """
     Thrown whenever a cart's item already exists.
     """
-    def __init__(self, status_code: int, message: str):
-        self.status_code = status_code
-        self.message = message
-        self.name = 'ItemAlreadyAddedException'
+    def __init__(self, message: str):
+        name = 'ItemAlreadyAddedException'
+        super().__init__(422, message, name)
 
-
-class ItemNotFoundException(Exception):
+class ItemNotFoundException(FastemplateBaseException):
     """
     Thrown whenever a cart's item is not found.
     """
-    def __init__(self, status_code: int, message: str):
-        self.status_code = status_code
-        self.message = message
-        self.name = 'ItemNotFoundException'
+    def __init__(self, message: str):
+        name = 'ItemNotFoundException'
+        super().__init__(404, message, name)
 
-
-class MismatchedLenghtException(Exception):
+class MismatchedLenghtException(FastemplateBaseException):
     """
     Thrown whenever a list has more/less items then prices, and vice versa.
     """
-    def __init__(self, status_code: int, message: str):
-        self.status_code = status_code
-        self.message = message
-        self.name = 'MismatchedLenghtException'
+    def __init__(self, message: str):
+        name = 'MismatchedLenghtException'
+        super().__init__(422, message, name)
