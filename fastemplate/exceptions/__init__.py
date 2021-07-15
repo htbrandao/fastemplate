ce0e5835fe9dd2b8f0127f8b9604cacb7ef507b1
Henrique Brandão <brandao.t.henrique@gmail.com>
2021-07-15 15:14:04 -0300

    Update __init__.py

--- fastemplate/exceptions/__init__.py
+++ fastemplate/exceptions/__init__.py
@@ -2,7 +2,7 @@ class FastemplateBaseException(Exception):
     """
     API Base exception
     """
-    def __init__(self, status_code: int, message: str):
+    def __init__(self, status_code: int, message: str, name: str):
         self.status_code = status_code
         self.message = message
-        self.name = 'FastemplateBaseException'
+        self.name = name
