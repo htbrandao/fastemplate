b79f8ca327ee52eb3203dca3f3a4876f9ce81ff4
Henrique Brandão <brandao.t.henrique@gmail.com>
2021-07-16 11:11:20 -0300

    Update __init__.py

--- fastemplate/utils/__init__.py
+++ fastemplate/utils/__init__.py
@@ -1,12 +1,12 @@
 from fastemplate.config import config
 
 
-def log_request_entries(log_file='log.log'):
+def log_request_entries(log_file='fastemplate.log'):
     """
     Retrieves the amount of log entries.
 
     :param str log_file: name of the log file
     :return: int
     """
-    lines = open(file=f'{config.ROOT_DIR}/log.log', mode='r').readlines()
+    lines = open(file=log_file, mode='r').readlines()
     return len(lines)
