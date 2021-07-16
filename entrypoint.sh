d281ad9cc24baa199c5a71721a166472e6816dcb
Henrique Brandão <brandao.t.henrique@gmail.com>
2021-07-16 10:42:07 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,4 @@
+#!/bin/bash
+
+# uvicorn fastemplate:app --host 0.0.0.0 --port 8000 --reload
 gunicorn -w 3 -b :8000 -k uvicorn.workers.UvicornWorker -t 90 --preload --max-requests=500 fastemplate:app
