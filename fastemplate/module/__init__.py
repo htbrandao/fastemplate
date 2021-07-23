d30af3a86719b9b17558182237b3ed76d469c993
Henrique Brandão <brandao.t.henrique@gmail.com>
2021-07-23 17:15:04 -0300

    Update __init__.py

--- fastemplate/module/__init__.py
+++ fastemplate/module/__init__.py
@@ -1,1 +1,17 @@
 BASE_CART = {}
+
+# ========================================================= #
+# Pre-load examples to try the api without sending new data #
+# ========================================================= #
+BASE_CART['breakfast'] = {
+    'milk': 5.0,
+    'capuccino': 3.0,
+    'banana': 2.5,
+}
+
+BASE_CART['dinner'] = {
+    'wine': 3.0,
+    'potato': 2.45,
+    'fish': 20.75,
+    'ice cream': 8.30
+}
