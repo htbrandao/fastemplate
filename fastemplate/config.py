659fdb0203a4ea2b157f55919ddaafcd9ea2e1dd
Henrique Brandão <brandao.t.henrique@gmail.com>
2021-07-26 15:37:19 -0300

    Update config.py

--- fastemplate/config.py
+++ fastemplate/config.py
@@ -1,11 +1,13 @@
 import os
 
-
 class Config:
     """
     Class to include variables related to the application.
     """
     ROOT_DIR = "/".join(os.path.dirname(os.path.abspath(__file__)).split("/")[:-1])
-
+    NAME = 'FASTEMPLATE'
+    DESCRIPTION = 'REST API Template to ease your understanding!'
+    ALLOW_CORS = True
+    SECURE_API = False
 
 config = Config()
