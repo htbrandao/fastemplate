a2eb379df5a5ad9c178db3f3602069f28b4ce2dc
Henrique Brandão <brandao.t.henrique@gmail.com>
2021-07-27 16:13:22 -0300

    Update __init__.py

--- fastemplate/services/v1/__init__.py
+++ fastemplate/services/v1/__init__.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
-from fastemplate.services.v1 import shopcart
+from fastemplate.services.v1 import shopcart, fridge
 
 v1 = APIRouter()
 
+v1.include_router(fridge.router, prefix='/fridge', tags=['fridge'])
 v1.include_router(shopcart.router, prefix='/shopcart', tags=['shopcart'])
