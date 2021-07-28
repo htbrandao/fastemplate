71ad1c2580ef58f55a204868d087629b1b174351
Henrique Brandão <brandao.t.henrique@gmail.com>
2021-07-28 15:58:06 -0300

    Update utils.py

--- fastemplate/services/utils.py
+++ fastemplate/services/utils.py
@@ -3,7 +3,11 @@ from starlette.middleware.cors import CORSMiddleware
 
 def allow_cors(app):
     """
-    # TODO: docstring
+    Enable CORS in app
+
+    :param FastAPI app: the main app
+    :return: app with CORS enabled
+    :rtype: FastAPI
     """
     return app.add_middleware(CORSMiddleware,
         allow_origins=["*"],
