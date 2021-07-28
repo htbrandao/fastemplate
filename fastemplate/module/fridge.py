8145a282abd3bd210162d4175bba7f9589b9a0a0
Henrique Brandão <brandao.t.henrique@gmail.com>
2021-07-28 15:55:11 -0300

    Update fridge.py

--- fastemplate/module/fridge.py
+++ fastemplate/module/fridge.py
@@ -2,4 +2,10 @@ from fastemplate.module import MOCK_FRIDGE
 
 
 def list_fridge():
+    """
+    List all items in the frigde.
+
+    :return: dict with all items and amounts
+    :rtype: dict
+    """
     return MOCK_FRIDGE
