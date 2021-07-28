2ea14d3599e1cf2c238dd116086c2be1c5fdb95d
Henrique Brandão <brandao.t.henrique@gmail.com>
2021-07-28 15:58:43 -0300

    Update fridge.py

--- fastemplate/services/v1/fridge.py
+++ fastemplate/services/v1/fridge.py
@@ -3,7 +3,6 @@ from fastapi.security import OAuth2PasswordBearer
 
 from fastemplate import logger
 from fastemplate.module import fridge
-from fastemplate.objects.user import User
 # from fastemplate.services.security import is_owner
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/login')
@@ -14,15 +13,24 @@ router = APIRouter()
 @router.get('/list_fridge', dependencies=[Depends(oauth2_scheme)])
 def list_fridge():
     """
-    # TODO: docstring
+    Endpoint. List fridge items.
+
+    :returns: dict with all frigde's items
+    :rtype: dict
     """
-    logger.info(f'Request@/list_fridge')
+    logger.info('Request@/list_fridge')
     return fridge.list_fridge()
 
 
+# TODO: dep is_owner, ban kitty from using
 # @router.post('/buy_all_tuna_there_is', dependencies=Depends(is_owner))
 @router.post('/buy_all_tuna_there_is')
 def buy_all_tuna():
-    # TODO: dep is_owner, ban kitty from using
-    logger.info(f'Request@/buy_all_tuna_there_is')
+    """
+    Endpoint. Buys all wolrd's tuna!!!
+
+    :returns: dict with message
+    :rtype: dict
+    """
+    logger.info('Request@/buy_all_tuna_there_is')
     return {'message': 'You\'ve successfully bought all tuna in the world!'}
