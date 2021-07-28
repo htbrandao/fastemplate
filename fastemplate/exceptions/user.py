f8e8ed1ee6cfb396a46dcffa36ac47e770f5f749
Henrique Brandão <brandao.t.henrique@gmail.com>
2021-07-28 18:12:41 -0300

    Update user.py

--- fastemplate/exceptions/user.py
+++ fastemplate/exceptions/user.py
@@ -1,9 +1,11 @@
 from fastemplate.exceptions import FastemplateBaseException
 
+
 class InvalidUsernameOrPassword(FastemplateBaseException):
     """
     Thrown whenever user credentials are wrong.
     """
+
     def __init__(self, message: str):
         name = 'InvalidUsernameOrPassword'
         super().__init__(400, message, name)
@@ -13,6 +15,7 @@ class NiceTryMeowNowGoBack(FastemplateBaseException):
     """
     Thrown whenever my cat tries to login as owner.
     """
+
     def __init__(self, message: str):
         name = 'NiceTryMeowNowGoBack'
         super().__init__(401, message, name)
@@ -22,6 +25,7 @@ class InvalidAuthCredentials(FastemplateBaseException):
     """
     Thrown whenever request has the correct token but invalid username or password.
     """
+
     def __init__(self, message: str):
         name = 'InvalidAuthCredentials'
         super().__init__(401, message, name)
