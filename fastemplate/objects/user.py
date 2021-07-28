52aa66a3613218ffe5fa979310cc2f1f970b07a8
Henrique Brandão <brandao.t.henrique@gmail.com>
2021-07-28 15:56:29 -0300

    Update user.py

--- fastemplate/objects/user.py
+++ fastemplate/objects/user.py
@@ -1,6 +1,9 @@
 from pydantic import BaseModel
 
 class User(BaseModel):
+    """
+    Schema to represent a user when dealing with authentication.
+    """
     username: str
     full_name: str
     owner: bool
