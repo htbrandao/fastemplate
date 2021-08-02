8bc8cc0865f878be3dcbcce500ef306df719fe77
Henrique Brandão <brandao.t.henrique@gmail.com>
2021-08-02 14:57:09 -0300

    Update fridge.py

--- fastemplate/module/fridge.py
+++ fastemplate/module/fridge.py
@@ -1,4 +1,9 @@
-from fastemplate.module import MOCK_FRIDGE
+from time import sleep
+from random import choice
+
+from fastapi.responses import StreamingResponse
+
+from fastemplate.module import MOCK_FRIDGE, MOCK_CELSIUS_TEMPERATURES
 
 
 def list_fridge():
@@ -9,3 +14,27 @@ def list_fridge():
     :rtype: dict
     """
     return MOCK_FRIDGE
+
+
+def get_temperature():
+    """
+    Yields a mock fridge temperature.
+    
+    return: fridge temperature encoded in utf-8
+    :rtype: bytes
+    """
+    for i in range(3):
+        yield bytes(choice(MOCK_CELSIUS_TEMPERATURES), encoding='utf-8')
+        sleep(1)
+
+
+def stream_temperature():
+    """
+    Streams the fridge temperature.
+
+    Encoded in `UTF-8`.
+
+    :returns: fridge's temperature.
+    :rtype: StreamingResponse
+    """
+    return StreamingResponse(get_temperature())
